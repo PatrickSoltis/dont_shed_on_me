a3179d880dbf79e67ea655ab2e2e5eeef2bd8ea5
PatrickSoltis <patrick.s.soltis@gmail.com>
2021-04-21 22:18:43 -0700

    First dump of constraints

--- MicrogridModel0_1.py
+++ MicrogridModel0_1.py
@@ -10,6 +10,9 @@ from cvxpy import *
 import pandas as pd
 
 # %%
+print('Hello world')
+
+# %%
 # DATA IMPORT
 office = pd.read_csv('SF_Office_import.csv') # Not planned in the microgrid
 CARE = pd.read_csv('CARE_import.csv')
@@ -68,6 +71,7 @@ plt.title("Critical Facility Load Profile: Disaggregated 10 Day Time Horizon")
 plt.xlabel("Time (Hours)")
 plt.ylabel("Power (kW)")
 plt.legend();
+
 # %%
 # Data Vis
 # Plot Solar curves
@@ -79,3 +83,99 @@ plt.xlabel("Time (Hours)")
 plt.ylabel("Power (kW)")
 plt.legend();
 # %%
+### Define optimization vars
+
+    ### nodal power
+
+# solar generation
+#s is a parameter
+p = Variable(7)
+q = Variable(7)
+
+# battery injection/absorption
+b_S = Variable(7)
+b_Q = Variable(7)
+b_P = Variable(7)
+
+# power delivered to load
+l_S = Variable(7)
+l_P = Variable(7)
+l_Q = Variable(7)
+
+# controllable generation (diesel)
+d_S = Variable(7)
+d_P = Variable(7)
+d_Q = Variable(7)
+
+# other
+V = Variable(7) #voltage
+F= Variable(7) #fraction of demand fulfilled
+j= Variable(7) #battery state of charge
+
+# power flow between nodes
+P = Variable((7, 7))
+Q = Variable((7, 7))
+L = Variable((7, 7))
+
+# %%
+### Define objective function
+objective = Maximize(R.T@F)
+
+# %%
+### Define constraints
+constraints = []
+
+    ### Reactive/Active/Apparent power definitions
+
+constraints += [ norm(vstack([p[jj],q[jj]])) <= s[jj] ] #solar p and q output tied to parameter input
+constraints += [ norm(vstack([b_P[jj],b_Q[jj]])) <= b_S[jj] ] #batteries
+constraints += [ norm(vstack([l_P[jj],l_Q[jj]])) <= l_S[jj] ] #demand
+constraints += [ norm(vstack([d_P[jj],d_Q[jj]])) <= d_S[jj] ] #diesel generator 
+
+### Following constraints are verified but not yet sorted by Patrick
+# Apparent Power Limits
+constraints = [s - s_max <= 0, -p <= 0, -q <= 0, norm(vstack([sum(p), sum(q)]))- sum(s)<= 0]
+#Not sure about this last one
+
+# Nodal voltage limits
+constraints += [v_min**2 - V <= 0, V - v_max**2 <= 0]
+
+# Squared line current limits
+constraints += [L - I_max**2 <= 0]
+
+# Boundary condition for power line flows
+constraints += [P[0,0] == 0, Q[0,0] == 0]
+
+# Boundary condition for squared line current
+constraints += [L[0,0] == 0]
+
+# Fix node 0 voltage to be 1 "per unit" (p.u.)
+constraints += [V[0] == 1]
+
+# Loop over each node
+for jj in j_idx:
+    
+    # Parent node, i = \rho(j)
+    i =  rho[jj]    
+    
+    # Line Power Flows
+    constraints += [P[i, jj] - (l_P[jj] - p[jj]) - r[i, jj]*L[i, jj] - sum([A[
+
+# %%
+# Additional inequality constraints: 0jjmax
+
+0 <= j <= jmax
+-b_Srating <= b_S[b] <= b_Srating
+b_S*dt <=  jt-1 
+0 <= b_S[d] <= dmax
+
+# Define constraints
+b_S[n] = 0
+F = l_S/D
+l_S = s + b_S        
+j[t] = j[t-1] - b_S[t]*dt   
+#j_0 = **choose initial value** A parameter?
+l_Sk = Dk
+
+0 <= b_Q 
+
