f13a9b7c66ecd0f36a973d7a85f768048ad6e8ca
PatrickSoltis <patrick.s.soltis@gmail.com>
2021-04-22 20:36:40 -0700

    constraints and variables dump

--- MicrogridModel0_1.py
+++ MicrogridModel0_1.py
@@ -10,6 +10,9 @@ from cvxpy import *
 import pandas as pd
 
 # %%
+print('Hello world')
+
+# %%
 # DATA IMPORT
 office = pd.read_csv('SF_Office_import.csv') # Not planned in the microgrid
 CARE = pd.read_csv('CARE_import.csv')
@@ -68,6 +71,7 @@ plt.title("Critical Facility Load Profile: Disaggregated 10 Day Time Horizon")
 plt.xlabel("Time (Hours)")
 plt.ylabel("Power (kW)")
 plt.legend();
+
 # %%
 # Data Vis
 # Plot Solar curves
@@ -79,109 +83,99 @@ plt.xlabel("Time (Hours)")
 plt.ylabel("Power (kW)")
 plt.legend();
 # %%
+### Define optimization vars
 
-# List of node indices
-i_idx = np.arange(8)
+    ### nodal power
 
-### Define microgrid parameters ###
+# solar generation
+#s is a parameter
+p = Variable(7)
+q = Variable(7)
 
-# Import nodal power demand values
-D = pd.read_csv("nodes.csv")
+# battery injection/absorption
+b_S = Variable(7)
+b_Q = Variable(7)
+b_P = Variable(7)
 
-#  Create apparent power demand dataframes
-D_apparent_total = pd.DataFrame()
-D_apparent_critical = pd.DataFrame()
-D_apparent_remaining = pd.DataFrame()
+# power delivered to load
+l_S = Variable(7)
+l_P = Variable(7)
+l_Q = Variable(7)
 
-#  Create active power demand dataframes
-D_real_total = pd.DataFrame()
-D_real_critical = pd.DataFrame()
-D_real_remaining = pd.DataFrame()
+# controllable generation (diesel)
+d_S = Variable(7)
+d_P = Variable(7)
+d_Q = Variable(7)
 
-#  Create reactive power demand dataframes
-D_reactive_total = pd.DataFrame()
-D_reactive_critical = pd.DataFrame()
-D_reactive_remaining = pd.DataFrame()
+# other
+V = Variable(7) #voltage
+F= Variable(7) #fraction of demand fulfilled
+j= Variable(7) #battery state of charge
 
-# Power demand [MW] at each node
-for x in i_idx:
-
-    # Apparent power dataframes
-    D_apparent_total["Node %1i"%x + " Total Apparent Power"] = D["Node %1i"%x + " (Apparent Power; Total)"]
-    D_apparent_critical["Node %1i"%x + " Critical Apparent Power"] = D["Node %1i"%x + " (Apparent Power; Critical)"]
-    D_apparent_remaining["Node %1i"%x + " Remaining Apparent Power"] = D["Node %1i"%x + " (Apparent Power; Remaining)"]
-
-    # Real power dataframes
-    D_real_total["Node %1i"%x + " Total Real Power"] = D["Node %1i"%x + " (Real Power; Total)"]
-    D_real_critical["Node %1i"%x + " Critical Real Power"] = D["Node %1i"%x + " (Real Power; Critical)"]
-    D_real_remaining["Node %1i"%x + " Remaining Real Power"] = D["Node %1i"%x + " (Real Power; Remaining)"]
-
-    # Reactive power dataframes
-    D_reactive_total["Node %1i"%x + " Total Reactive Power"] = D["Node %1i"%x + " (Reactive Power; Total)"]
-    D_reactive_critical["Node %1i"%x + " Critical Reactive Power"] = D["Node %1i"%x + " (Reactive Power; Critical)"]
-    D_reactive_remaining["Node %1i"%x + " Remaining Reactive Power"] = D["Node %1i"%x + " (Reactive Power; Remaining)"]
-
-# Create solar power generated dataframe
-s_s = pd.DataFrame()
-
-# Define month of solar power to be examined
-month = "June"
-
-# Solar apparent power generated at each node (excludes power dispatched from batteries)
-s_s["Node 0: " + month + " solar generation"] = 0
-s_s["Node 1: " + month + " solar generation"] = solar[month + " (kW)"]
-s_s["Node 2: " + month + " solar generation"] = solar[month + " (kW)"]
-
-for x2 in np.arange(3,8):
-    s_s["Node %1i: "%x2 + month + " solar generation"] = 0
-s_s["Node 0: " + month + " solar generation"] = 0
-
-# Solar real power generated at each node (excludes power dispatched from batteries)
-s_p = s_s
-
-# Solar reactive power generated at each node (excludes power dispatched from batteries)
-s_q = s_s
-s_q.iloc[:,:] = 0
-
-# Maximum energy that batteries at nodes 1,2 and 7 can store (battery capacity)
-j_max = np.array([9.5, 9.5, 95])
-
-# Maximum power discharge and charge rates
-bS_rating = np.array([[25, 25, 40],
-                      [25, 25, 40]])
-
-# Maximum power that diesel generator can produce (kW)
-d_max = 20
-
-# Priority ranking of different customer categories (5=highest priority, 1=lowest)
-R = np.array([5, 4, 3, 2])
-
-# Minimum nodal voltage
-V_min = 0.95
-
-# Maximum nodal voltage
-V_max = 1.05
-
-# Resistance of each power line (between a node and its parent node)
-r = np.array([0, 0, 0, 0, 0, 0, 0])
-
-# Reactance of each power line (between a node and its parent node)
-x = np.array([0, 0, 0, 0, 0, 0, 0])
-
-# Maximum current for each power line (between a node and its parent node)
-I_max = np.array([0, 0, 0, 0, 0, 0, 0])
-
-# Adjacency matrix where A[i, j]=1 if i is the parent of j
-A = np.array([[1, 1, 1, 1, 1, 1, 1, 1],
-              [0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0]])
-
-# rho(j): Parent node of node j
-rho = np.array([0, 0, 0, 0, 0, 0, 0, 0])
+# power flow between nodes
+P = Variable((7, 7))
+Q = Variable((7, 7))
+L = Variable((7, 7))
 
 # %%
+### Define objective function
+objective = Maximize(R.T@F)
+
+# %%
+### Define constraints
+constraints = []
+
+    ### Reactive/Active/Apparent power definitions
+
+constraints += [ norm(vstack([p[jj],q[jj]])) <= s[jj] ] #solar p and q output tied to parameter input
+constraints += [ norm(vstack([b_P[jj],b_Q[jj]])) <= b_S[jj] ] #batteries
+constraints += [ norm(vstack([l_P[jj],l_Q[jj]])) <= l_S[jj] ] #demand
+constraints += [ norm(vstack([d_P[jj],d_Q[jj]])) <= d_S[jj] ] #diesel generator 
+
+### Following constraints are verified but not yet sorted by Patrick
+# Apparent Power Limits
+constraints = [s - s_max <= 0, -p <= 0, -q <= 0, norm(vstack([sum(p), sum(q)]))- sum(s)<= 0]
+#Not sure about this last one
+
+# Nodal voltage limits
+constraints += [v_min**2 - V <= 0, V - v_max**2 <= 0]
+
+# Squared line current limits
+constraints += [L - I_max**2 <= 0]
+
+# Boundary condition for power line flows
+constraints += [P[0,0] == 0, Q[0,0] == 0]
+
+# Boundary condition for squared line current
+constraints += [L[0,0] == 0]
+
+# Fix node 0 voltage to be 1 "per unit" (p.u.)
+constraints += [V[0] == 1]
+
+# Loop over each node
+for jj in j_idx:
+    
+    # Parent node, i = \rho(j)
+    i =  rho[jj]    
+    
+    # Line Power Flows
+    constraints += [P[i, jj] - (l_P[jj] - p[jj]) - r[i, jj]*L[i, jj] - sum([A[
+
+# %%
+# Additional inequality constraints: 0jjmax
+
+0 <= j <= jmax
+-b_Srating <= b_S[b] <= b_Srating
+b_S*dt <=  jt-1 
+0 <= b_S[d] <= dmax
+
+# Define constraints
+b_S[n] = 0
+F = l_S/D
+l_S = s + b_S        
+j[t] = j[t-1] - b_S[t]*dt   
+#j_0 = **choose initial value** A parameter?
+l_Sk = Dk
+
+0 <= b_Q 
+
