a6d92145fd0e38ff197699613ae5937d5c3c8cce
Patrick Soltis <44043164+PatrickSoltis@users.noreply.github.com>
2021-04-22 20:38:09 -0700

    Accidentally deleted Jack's stuff, re-added
    Should be intended state from start of last meeting

--- MicrogridModel0_1.py
+++ MicrogridModel0_1.py
@@ -82,6 +82,111 @@ plt.title("Solar Gen: 10 Day Time Horizon")
 plt.xlabel("Time (Hours)")
 plt.ylabel("Power (kW)")
 plt.legend();
+
+# List of node indices
+i_idx = np.arange(8)
+
+### Define microgrid parameters ###
+
+# Import nodal power demand values
+D = pd.read_csv("nodes.csv")
+
+#  Create apparent power demand dataframes
+D_apparent_total = pd.DataFrame()
+D_apparent_critical = pd.DataFrame()
+D_apparent_remaining = pd.DataFrame()
+
+#  Create active power demand dataframes
+D_real_total = pd.DataFrame()
+D_real_critical = pd.DataFrame()
+D_real_remaining = pd.DataFrame()
+
+#  Create reactive power demand dataframes
+D_reactive_total = pd.DataFrame()
+D_reactive_critical = pd.DataFrame()
+D_reactive_remaining = pd.DataFrame()
+
+# Power demand [MW] at each node
+for x in i_idx:
+
+    # Apparent power dataframes
+    D_apparent_total["Node %1i"%x + " Total Apparent Power"] = D["Node %1i"%x + " (Apparent Power; Total)"]
+    D_apparent_critical["Node %1i"%x + " Critical Apparent Power"] = D["Node %1i"%x + " (Apparent Power; Critical)"]
+    D_apparent_remaining["Node %1i"%x + " Remaining Apparent Power"] = D["Node %1i"%x + " (Apparent Power; Remaining)"]
+
+    # Real power dataframes
+    D_real_total["Node %1i"%x + " Total Real Power"] = D["Node %1i"%x + " (Real Power; Total)"]
+    D_real_critical["Node %1i"%x + " Critical Real Power"] = D["Node %1i"%x + " (Real Power; Critical)"]
+    D_real_remaining["Node %1i"%x + " Remaining Real Power"] = D["Node %1i"%x + " (Real Power; Remaining)"]
+
+    # Reactive power dataframes
+    D_reactive_total["Node %1i"%x + " Total Reactive Power"] = D["Node %1i"%x + " (Reactive Power; Total)"]
+    D_reactive_critical["Node %1i"%x + " Critical Reactive Power"] = D["Node %1i"%x + " (Reactive Power; Critical)"]
+    D_reactive_remaining["Node %1i"%x + " Remaining Reactive Power"] = D["Node %1i"%x + " (Reactive Power; Remaining)"]
+
+# Create solar power generated dataframe
+s_s = pd.DataFrame()
+
+# Define month of solar power to be examined
+month = "June"
+
+# Solar apparent power generated at each node (excludes power dispatched from batteries)
+s_s["Node 0: " + month + " solar generation"] = 0
+s_s["Node 1: " + month + " solar generation"] = solar[month + " (kW)"]
+s_s["Node 2: " + month + " solar generation"] = solar[month + " (kW)"]
+
+for x2 in np.arange(3,8):
+    s_s["Node %1i: "%x2 + month + " solar generation"] = 0
+s_s["Node 0: " + month + " solar generation"] = 0
+
+# Solar real power generated at each node (excludes power dispatched from batteries)
+s_p = s_s
+
+# Solar reactive power generated at each node (excludes power dispatched from batteries)
+s_q = s_s
+s_q.iloc[:,:] = 0
+
+# Maximum energy that batteries at nodes 1,2 and 7 can store (battery capacity)
+j_max = np.array([9.5, 9.5, 95])
+
+# Maximum power discharge and charge rates
+bS_rating = np.array([[25, 25, 40],
+                      [25, 25, 40]])
+
+# Maximum power that diesel generator can produce (kW)
+d_max = 20
+
+# Priority ranking of different customer categories (5=highest priority, 1=lowest)
+R = np.array([5, 4, 3, 2])
+
+# Minimum nodal voltage
+V_min = 0.95
+
+# Maximum nodal voltage
+V_max = 1.05
+
+# Resistance of each power line (between a node and its parent node)
+r = np.array([0, 0, 0, 0, 0, 0, 0])
+
+# Reactance of each power line (between a node and its parent node)
+x = np.array([0, 0, 0, 0, 0, 0, 0])
+
+# Maximum current for each power line (between a node and its parent node)
+I_max = np.array([0, 0, 0, 0, 0, 0, 0])
+
+# Adjacency matrix where A[i, j]=1 if i is the parent of j
+A = np.array([[1, 1, 1, 1, 1, 1, 1, 1],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0]])
+
+# rho(j): Parent node of node j
+rho = np.array([0, 0, 0, 0, 0, 0, 0, 0])
+
 # %%
 ### Define optimization vars
 
