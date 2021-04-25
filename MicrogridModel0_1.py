07ca224e3c4073080e2a626d462d1d55d9a3a2db
PatrickSoltis <patrick.s.soltis@gmail.com>
2021-04-25 00:26:56 -0700

    Added optimization variables

--- MicrogridModel0_1.py
+++ MicrogridModel0_1.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as pyplot
 from cvxpy import *
 import pandas as pd
+
 #%% Data Imports
 
 #Apparent power values assume pf=0.95
@@ -12,6 +13,7 @@ a10 = pd.read_csv('a10_import.csv') #Fire station
 e1 = pd.read_csv('e1_import.csv') #Residential
 mbl = pd.read_csv('MBL_import.csv') #Medical baseline ???
 solar = pd.read_csv('solar_import.csv')
+
 #%% Parameters A - Direct Objective Inputs (1-2)
 
 D_df = pd.read_csv('nodes.csv')
@@ -34,6 +36,7 @@ for item in D_df.columns:
     i += 1
     print(i, item)
 '''
+
 # %% Parameters B (3-8)
 
 #3 - solar PV generation
@@ -60,6 +63,7 @@ d_rating = np.array([0, 0, 10, 0, 0, 0, 0, 0])
 #8 - voltage limits
 V_min = 0.95
 V_max = 1.05
+
 # %% Parameters C (9-11)
 
 #9 - Resistance and reactance
@@ -79,8 +83,76 @@ A = np.array([[1, 1, 1, 1, 1, 1, 1, 1],
               [0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0, 0]])
 #A[i, j]=1 if i is the parent of j
+
+#Rho - Defines parent nodes in for loops
+rho = np.array([0, 0, 0, 0, 0, 0, 0, 0])
+
 #%% Parameters D (12-)
 
 #12 - Time step
 dt = 1
+
+#13 - Inverter efficiencies
+nu_s = 1 #solar
+nu_b = 1 #battery
+#Efficiency already included in generator output. 
+#Only matters for solar because solar input is total solar energy,
+#and for batteries because energy put in and out of system needs to be in terms of system.
+#Note values are not yet built into constraints
+
+# %% Optimization Variables
+
+#Define number of time steps
+t = 1
+
+#1 - Fraction of load served
+F = Variable((8,t))
+
+#2 - Load supplied (apparent, real, reactive)
+l_S = Variable((8,t))
+l_P = Variable((8,t))
+l_Q = Variable((8,t))
+
+#3 - Battery power dispatched (+) or stored (-)
+b_S = Variable((8,t))
+b_P = Variable((8,t))
+b_Q = Variable((8,t))
+
+#4 - Diesel power generated
+d_S = Variable((8,t))
+d_P = Variable((8,t))
+d_Q = Variable((8,t))
+
+#5 - Solar power (real and reactive)
+s_P = Variable((8,t))
+s_Q = Variable((8,t))
+#Currently solar only provides real power: s_P = s_S (in constraints)
+#So these are duplicative, but relevant if modeling inverter that can provide P and Q
+
+#6 - Net power
+s = Variable((8,t))
+p = Variable((8,t))
+q = Variable((8,t))
+
+#7 - Voltage
+V = Variable((8,t))
+
+#8 - Energy stock
+j = Variable((8,t)) #Battery state of charge
+f = Variable((8,t)) #Diesel fuel available
+
+#9-11 - Line variable
+P = Variable((8,t)) #active power flow
+Q = Variable((8,t)) #reactive power flow
+L = Variable((8,t)) #squared magnitude of complex current
+
+'''
+#9-11 - Line  variables
+P = Variable((8,8,t)) #active power flow
+Q = Variable((8,8,t)) #reactive power flow
+L = Variable((8,8,t)) #squared magnitude of complex current
+# ValueError: Expressions of dimension greater than 2 are not supported.
+# Alternative version (above) squeezes into 2 dimensions since all nodes 1-7 have parent 0. 
+'''
+
 # %%
