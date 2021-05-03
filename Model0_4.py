90abd0c81df10c579c32b6d396ac48b7b351e530
Patrick Soltis <44043164+PatrickSoltis@users.noreply.github.com>
2021-05-03 11:30:21 -0700

    Data imports & Parameters A from version 0_3

--- Model0_4.py
+++ Model0_4.py
@@ -1,1 +1,34 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as pyplot
+from cvxpy import *
+import pandas as pd
 
+#%% DATA IMPORTS
+
+#Apparent power values assume pf=0.95
+office = pd.read_csv('SF_Office_import.csv') #Office building - not used in model
+CARE = pd.read_csv('CARE_import.csv') #CARE/FERA residential
+a10 = pd.read_csv('a10_import.csv') #Fire station
+e1 = pd.read_csv('e1_import.csv') #Residential
+mbl = pd.read_csv('MBL_import.csv') #Medical baseline ???
+solar = pd.read_csv('solar_import.csv')
+
+#%% PARAMETERS A - Direct Objective Inputs (1-2)
+
+D_df = pd.read_csv('nodes.csv')
+
+#1 - Value ranking of different customer categories (5=highest priority, 1=lowest)
+R = np.array([[0, 0, 0, 0, 4, 3, 2, 1]])
+#Remaining length-8 arrays are only 1 dimension. 
+# They will only be used in constraints at a single time.
+
+#Select hour numbers to use from load and solar data
+t0 = 12
+len_t = 3
+
+#2 - Load total power for each node into D
+D = D_df.iloc[t0:t0+len_t, [1,10,19,28,37,46,55,64]].to_numpy()
+D_P = D_df.iloc[t0:t0+len_t, [4,13,22,31,40,49,58,67]].to_numpy()
+D_Q = D_df.iloc[t0:t0+len_t, [7,16,25,34,43,52,61,70]].to_numpy()
+#Index as: D[hour number, node number]
