2ae573c2c7b69f0c6f5e2409dfc407c09cccf5e1
Patrick Soltis <44043164+PatrickSoltis@users.noreply.github.com>
2021-05-03 11:38:51 -0700

    Adding energy availability comparison

--- TestOutputs0_1.py
+++ TestOutputs0_1.py
@@ -1,7 +1,14 @@
 #1 Provides a numbered index for all columns in D_df
+#Prerequisites: Parameters A definitions
 i = -1
 for item in D_df.columns:
     i += 1
     print(i, item)
-    
-  
+
+#2 Compare energy available in system to demand
+#Prerequisites: Parameters A&B definitions
+print('D:',np.sum(D))
+print('s_S:',np.sum(s_S))
+print('j_start:',np.sum(j_start))
+print('f_start:',np.sum(f_start))
+print('energy available:',np.sum([np.sum(s_S),np.sum(j_start),np.sum(f_start)]))
